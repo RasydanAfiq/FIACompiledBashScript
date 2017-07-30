509f143a936162f70d2ca139f381e146283d1d63
Fhadila <1506164A@student.tp.edu.sg>
2017-07-30 22:14:17 +0800

    Script Compiled 6.2.1.10-6.2.1.21

--- remedial.sh
+++ remedial.sh
@@ -1484,3 +1484,306 @@ printf "\n\n"
 #To capture escaped strings and close the terminal
 read -n 1 -s -r -p "Press any key to exit!"
 kill -9 $PPID
+
+# ! /bin/bash
+
+#6.2.1.10
+loginfail=`grep "\-w /var/log/faillog -p wa -k logins" /etc/audit/audit.rules`
+loginlast=`grep "\-w /var/log/lastlog -p wa -k logins" /etc/audit/audit.rules`
+logintally=`grep "\-w /var/log/tallylog -p wa -k logins" /etc/audit/audit.rules`
+
+if [ -z "$loginfail" -o -z "$loginlast" -o -z "$logintally" ]
+then
+	if [ -z "$loginfail" ]
+	then
+		echo "-w /var/log/faillog -p wa -k logins" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$loginlast" ]
+	then
+		echo "-w /var/log/lastlog -p wa -k logins" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$logintally" ]
+	then
+		echo "-w /var/log/tallylog -p wa -k logins" >> /etc/audit/audit.rules
+	fi
+fi
+	
+pkill -P 1 -HUP auditd
+
+#6.2.1.11
+sessionwtmp=`egrep '\-w /var/log/wtmp -p wa -k session' /etc/audit/audit.rules`
+sessionbtmp=`egrep '\-w /var/log/btmp -p wa -k session' /etc/audit/audit.rules`
+sessionutmp=`egrep '\-w /var/run/utmp -p wa -k session' /etc/audit/audit.rules`
+
+if [ -z "$sessionwtmp" -o -z "$sessionbtmp" -o -z "$sessionutmp" ]
+then 
+	if [ -z "$sessionwtmp"]
+	then 
+		echo "-w /var/log/wtmp -p wa -k session" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$sessionbtmp"]
+	then 
+		echo "-w /var/log/btmp -p wa -k session" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$sessionutmp"]
+	then
+		echo "-w /var/run/utmp -p wa -k session" >> /etc/audit/audit.rules
+	fi
+fi
+
+pkill -HUP -P 1 auditd
+
+#6.2.1.12
+permission1=`grep "\-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission2=`grep "\-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission3=`grep "\-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission4=`grep "\-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission5=`grep "\-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -Fauid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+permission6=`grep "\-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+if [ -z "$permission1" -o -z "$permission2" -o -z permission3 -o -z permission4 -o -z permission5 -o -z permission6  ]
+then 
+	if [ -z "$permission1" ]
+	then
+		echo "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+
+	if [ -z "$permission2" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$permission3" ]
+	then 
+		echo "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$permission4" ]
+	then
+		echo "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$permission5" ]
+	then 
+		echo "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$permission6" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> /etc/audit/audit.rules
+
+	fi
+fi
+pkill -P 1 -HUP auditd
+
+#6.2.1.13
+access1=`grep "\-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access2=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access3=`grep "\-a always,exit -F arch=b64 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access4=`grep "\-a always,exit -F arch=b32 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access5=`grep "\-a always,exit -F arch=b32 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+access6=`grep "\-a always,exit -F arch=b32 -S creat -S open -S ope
+nat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+if [ -z "$access1" -o -z "$access2" ]
+then
+	if [ -z "$access1" ]
+	then     
+   		echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$access2" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" >> /etc/audit/audit.rules
+	fi
+	if [ -z "$access3" ]
+	then
+		echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+	if [ -z "$access4" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+	if [ -z "$access5" ]
+	then
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+	if [ -z "$access6" ]
+	then 
+		echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" >>  /etc/audit/audit.rules
+	fi
+fi
+
+pkill -P 1 -HUP auditd
+
+#6.2.1.14 Collect Use of Privileged Commands
+find / -xdev \( -perm -4000 -o -perm -2000 \) -type f | awk '{print "-a always,exit-F path=" $1 " -F perm=x -F auid>=1000 -F auid!=4294967295 -k privileged" }' > /tmp/1.log
+checkprivilege=`cat /tmp/1.log`
+cat /etc/audit/audit.rules | grep -- "$checkprivilege" > /tmp/2.log 
+checkprivilegenotinfile=`grep -F -x -v -f /tmp/2.log /tmp/1.log`
+
+if [ -n "$checkprivilegenotinfile" ]
+then
+	echo "$checkprivilegenotinfile" >> /etc/audit/audit.rules
+fi
+
+rm /tmp/1.log
+rm /tmp/2.log
+
+#6.2.1.15 Collect Successful File System Mounts
+bit64mountb64=`grep "\-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit64mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit32mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+
+if [ -z "$bit64mountb64" ]
+then
+	echo "-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/audit.rules
+fi
+
+if [ -z "$bit64mountb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/audit.rules
+fi
+
+pkill -HUP -P 1 auditd
+
+if [ -z "$bit32mountb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> /etc/audit/audit.rules
+fi
+
+pkill -HUP -P 1 auditd
+
+#2.6.1.16 Collect File Delection Events by User
+bit64delb64=`grep "\-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit64delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit32delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+
+if [ -z "$bit64delb64" ]
+then
+	echo "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/audit.rules
+fi
+
+if [ -z "$bit64delb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/audit.rules
+fi
+
+pkill -HUP -P 1 auditd
+
+if [ -z "$bit32delb32" ]
+then
+	echo "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> /etc/audit/audit.rules
+fi
+
+pkill -P 1 -HUP auditd
+
+#6.2.1.17 Collect Changes to System Administrator Scope
+sudoers=`grep "\-w /etc/sudoers -p wa -k scope" /etc/audit/audit.rules`
+
+if [ -z "$sudoers" ]
+then
+	echo "-w /etc/sudoers -p wa -k scope" >> /etc/audit/audit.rules
+fi
+pkill -HUP -P 1 auditd
+
+#6.2.1.18
+remauditrules=`grep actions /etc/audit/audit.rules`
+auditrules='-w /var/log/sudo.log -p wa -k actions'
+
+if [ -z "$remauditrules" -o "$remauditrules" != "$auditrules" ] 
+then
+	echo "$auditrules" >> /etc/audit/audit.rules
+fi
+
+pkill -HUP -P 1 auditd
+
+#6.2.1.19
+remmod1=`grep "\-w /sbin/insmod -p x -k modules" /etc/audit/audit.rules`
+remmod2=`grep "\-w /sbin/rmmod -p x -k modules" /etc/audit/audit.rules`
+remmod3=`grep "\-w /sbin/modprobe -p x -k modules" /etc/audit/audit.rules`
+remmod4=`grep "\-a always,exit -F arch=b64 -S init_module -S delete_module -k modules" /etc/audit/audit.rules`
+
+if [ -z "$remmod1" -o -z "$remmod2" -o -z "$remmod3" -o -z "$remmod4" -o -z "$remmod5" ]
+then
+	if [ -z "$remmod1" ]
+	then
+		echo "-w /sbin/insmod -p x -k modules" >> /etc/audit/audit.rules
+	fi
+
+	if [ -z "$remmod2" ]
+	then	
+		echo "-w /sbin/rmmod -p x -k modules" >> /etc/audit/audit.rules
+	fi
+
+	if [ -z "$remmod3" ]
+	then
+		echo "-w /sbin/modprobe -p x -k modules" >> /etc/audit/audit.rules
+	fi
+
+	if [ -z "$remmod4" ]
+	then
+		echo "-a always,exit -F arch=b64 -S init_module -S delete_module -k modules" >> /etc/audit/audit.rules
+	fi
+fi
+
+#6.2.1.20
+remimmute=`grep "^-e 2" /etc/audit/audit.rules`
+immute='-e 2'
+
+if [ -z "$remimmute" -o "$remimmute" != "$immute" ]
+then
+	echo "$immute" >> /etc/audit/audit.rules
+fi
+
+#6.2.1.21
+remlogrotate=`grep "/var/log" /etc/logrotate.d/syslog`
+logrotate='/var/log/messages /var/log/secure /var/log/maillog /var/log/spooler /var/log/boot.log /var/log/cron {'
+
+if [ -z "$remlogrotate" -o "$remlogrotate" != "$logrotate" ]
+then
+	rotate1=`grep "/var/log/messages" /etc/logrotate.d/syslog`
+	rotate2=`grep "/var/log/secure" /etc/logrotate.d/syslog`
+	rotate3=`grep "/var/log/maillog" /etc/logrotate.d/syslog`
+	rotate4=`grep "/var/log/spooler" /etc/logrotate.d/syslog`
+	rotate5=`grep "/var/log/boot.log" /etc/logrotate.d/syslog`
+	rotate6=`grep "/var/log/cron" /etc/logrotate.d/syslog`
+	
+	if [ -z "$rotate1" ]
+	then
+		echo "/var/log/messages" >> /etc/logrotate.d/syslog
+	fi
+
+	if [ -z "$rotate2" ]
+	then
+		echo "/var/log/secure" >> /etc/logrotate.d/syslog
+	fi
+
+	if [ -z "$rotate3" ]
+	then 
+		echo "/var/log/maillog" >> /etc/logrotate.d/syslog
+	fi
+
+	if [ -z "$rotate4" ]
+	then
+		echo "/var/log/spooler" >> /etc/logrotate.d/syslog
+	fi
+
+	if [ -z "$rotate5" ]
+	then
+		echo "/var/log/boot.log" >> /etc/logrotate.d/syslog
+	fi
+
+	if [ -z "$rotate6" ]
+	then
+		echo "/var/log/cron" >> /etc/logrotate.d/syslog
+	fi
+fi
--- verify.sh
+++ verify.sh
@@ -1045,3 +1045,171 @@ printf "\n\n"
 # Force exit
 read -n 1 -s -r -p "Press any key to exit!"
 kill -9 $PPID
+
+#! /bin/bash
+
+#6.2.1.10
+chklogins=`grep logins /etc/audit/audit.rules`
+loginfail=`grep "\-w /var/log/faillog -p wa -k logins" /etc/audit/audit.rules`
+loginlast=`grep "\-w /var/log/lastlog -p wa -k logins" /etc/audit/audit.rules`
+logintally=`grep "\-w /var/log/tallylog -p wa -k logins" /etc/audit/audit.rules`
+
+if [ -z "$loginfail" -o -z "$loginlast" -o -z "$logintally" ]
+then
+        echo "FAILED - Login and logout events not recorded."
+else
+        echo "PASSED - Login and logout events recorded."
+fi
+
+#6.2.1.11
+chksession=`egrep 'wtmp|btmp|utmp' /etc/audit/audit.rules`
+sessionwtmp=`egrep "\-w /var/log/wtmp -p wa -k session" /etc/audit/audit.rules`
+sessionbtmp=`egrep "\-w /var/log/btmp -p wa -k session" /etc/audit/audit.rules`
+sessionutmp=`egrep "\-w /var/run/utmp -p wa -k session" /etc/audit/audit.rules`
+
+if [ -z "$sessionwtmp" -o -z "$sessionbtmp" -o -z "sessionutmp" ]
+then
+        echo "FAILED - Session initiation information not collected."
+else
+        echo "PASSED - Session initiation information is collected."
+fi
+
+#6.2.1.12
+chkpermission64=`grep perm_mod /etc/audit/audit.rules`
+permission1=`grep "\-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission2=`grep "\-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F
+auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission3=`grep "\-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission4=`grep "\-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S|chown -F auid>=1000 -F auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission5=`grep "\-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -Fauid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+permission6=`grep "\-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S
+ fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F
+auid!=4294967295 -k perm_mod" /etc/audit/audit.rules`
+
+if [ -z "$permission1" -o -z "$permission2" -o -z permission3 -o -z permission4 -o -z permission5 -o -z permission6 ]
+then
+        echo "FAILED - Permission modifications not recorded."
+
+else
+        echo "PASSED - Permission modification are recorded."
+fi
+
+#6.2.1.13
+chkaccess=`grep access /etc/audit/audit.rules`
+access1=`grep "\-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access2=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access3=`grep "\-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access4=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access5=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+access6=`grep "\-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 - k access" /etc/audit/audit.rules`
+
+if [ -z "$access1" -o -z "$access2" -o -z "$access3" -o -z "$access4" -o -z "$access5" -o -z "$access6" ]
+then
+        echo "FAILED - Unsuccesful attempts to access files."
+
+else
+        echo "PASSED - Successful attempts to access files."
+fi
+
+#6.2.1.14 Collect Use of Privileged Commands
+find / -xdev \( -perm -4000 -o -perm -2000 \)  -type f | awk '{print "-a always, exit-F path=" $1 " -F perm=x -F auid>=1000 -F auid!=4294967295 -k privileged" }' > /tmp/1.log
+
+checkprivilege=`cat /tmp/1.log`
+cat /etc/audit/audit.rules | grep -- "$checkprivilege" > /tmp/2.log
+
+checkprivilegenotinfile=`grep -F -x -v -f /tmp/2.log /tmp/1.log`
+
+if [ -n "$checkprivilegenotinfile" ]
+then
+	echo "FAILED - Privileged Commands not in audit"
+else
+	echo "PASSED - Privileged Commands in audit"
+fi
+
+rm /tmp/1.log
+rm /tmp/2.log 
+
+#6.2.1.15 Collect Successful File System Mounts
+bit64mountb64=`grep "\-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit64mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+bit32mountb32=`grep "\-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" /etc/audit/audit.rules`
+
+if [ -z "$bit64mountb64" -o -z "$bit64mountb32" -o -z "$bit32mountb32" ]
+then
+	echo "FAILED - To determine filesystem mounts" 
+else
+	echo "PASSED - To determine filesystem mounts"
+fi
+
+#6.2.1.16 Collect File Delection Events by User
+bit64delb64=`grep "\-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit64delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+bit32delb32=`grep "\-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" /etc/audit/audit.rules`
+
+if [ -z "$bit64delb64" -o -z "$bit64delb32" -o -z "$bit32delb32" ]
+then
+	echo "FAILED - To determine the file delection event by user"
+else
+	echo "PASSED - To determine the file delection event by user"
+fi
+
+#6.2.1.17 Collect Changes to System Administration Scope
+chkscope=`grep scope /etc/audit/audit.rules`
+sudoers='-w /etc/sudoers -p wa -k scope'
+
+if [ -z "$chkscope" -o "$chkscope" != "$sudoers" ]
+then
+	echo "FAILED - To unauthorize change to scope of system administrator activity"
+else
+	echo "PASSED - To unauthorize change to scope of system administrator activity"
+fi
+
+#6.2.1.18 
+chkadminrules=`grep actions /etc/audit/audit.rules`
+adminrules='-w /var/log/sudo.log -p wa -k actions'
+
+if [ -z "$chkadminrules" -o "$chkadminrules" != "$adminrules" ]
+then 
+	echo "FAILED - Administrator activity not recorded"
+else
+	echo "PASSED - Administrator activity recorded"
+fi
+
+#6.2.1.19
+chkmod1=`grep "\-w /sbin/insmod -p x -k modules" /etc/audit/audit.rules`
+chkmod2=`grep "\-w /sbin/rmmod -p x -k modules" /etc/audit/audit.rules`
+chkmod3=`grep "\-w /sbin/modprobe -p x -k modules" /etc/audit/audit.rules`
+chkmod4=`grep "\-a always,exit -F arch=b64 -S init_module -S delete_module -k modules" /etc/audit/audit.rules`
+
+if [ -z "$chkmod1" -o -z "$chkmod2" -o -z "$chkmod3" -o -z "$chkmod4" ]
+then
+	echo "FAILED - Kernel module not recorded"
+else
+	echo "PASSED - Kernel module recorded"
+fi
+
+#6.2.1.20
+chkimmute=`grep "^-e 2" /etc/audit/audit.rules`
+immute='-e 2'
+
+if [ -z "$chkimmute" -o "$chkimmute" != "$immute" ]
+then
+	echo "FAILED - Audit configuration is not immutable"
+else
+	echo "PASSED - Audit configuration immutable"
+fi
+
+#6.2.1.21
+chkrotate1=`grep "/var/log/messages" /etc/logrotate.d/syslog`
+chkrotate2=`grep "/var/log/secure" /etc/logrotate.d/syslog`
+chkrotate3=`grep "/var/log/maillog" /etc/logrotate.d/syslog`
+chkrotate4=`grep "/var/log/spooler" /etc/logrotate.d/syslog`
+chkrotate5=`grep "/var/log/boot.log" /etc/logrotate.d/syslog`
+chkrotate6=`grep "/var/log/cron" /etc/logrotate.d/syslog`
+
+if [ -z "chkrotate1" -o -z "$chkrotate2" -o -z "$chkrotate3" -o -z "$chkrotate4" -o -z "$chkrotate5" -o -z "$chkrotate6" ]
+then
+	echo "FAILED - System logs not rotated"
+else
+	echo "PASSED - System logs recorded"
+fi
